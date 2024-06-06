2aff84b17ce170ace6c122914e9bcea0686bf7b6
johnolek <johnoleksowicz@icloud.com>
2024-06-06 13:12:59 -0500

    dynamically generate board and piece options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,30 @@ class ApplicationController < ActionController::Base
 
   def daily_games
     @username = params[:username].gsub(/[^a-zA-Z0-9_-]/, '')
+    @board_options = board_options
+    @piece_options = directory_names(Rails.root.join('public', 'piece', '*'))
+  end
+
+
+  private
+
+  def directory_names(path)
+    Dir.glob(path).select do |path|
+      File.directory?(path)
+    end.map do |dir|
+      File.basename(dir)
+    end.uniq
+  end
+
+  def board_options
+    data_board_values = Set.new
+    File.open(Rails.root.join('public', 'css', 'lichess_site.css'), 'r') do |f|
+      f.each_line do |line|
+        match = line.match(/data-board=(\w+)\]/)
+        data_board_values.add(match[1]) if match
+      end
+    end
+    data_board_values
   end
 
 end
