0922d32477c77a2be5eb99a2a6d85a3ec4f19916
johnolek <johnoleksowicz@icloud.com>
2024-06-07 15:34:46 -0500

    Save static version of board/piece options

---
+++ app/javascript/src/board/options.js
@@ -0,0 +1,5 @@
+const boardOptions = ["blue","blue2","blue3","brown","canvas","cocoa","green","grey","horsey","ic","leather","maple","maple2","marble","metal","newspaper","olive","parchment","pink","purple","wood","wood2","wood3","wood4"];
+
+const pieceSetOptions =  ["alpha","anarcandy","caliente","california","cardinal","cburnett","celtic","chess7","chessnut","companion","disguised","dubrovny","fantasy","fresca","gioco","governor","horsey","icpieces","kiwen-suwi","kosal","leipzig","letter","libra","maestro","merida","mono","mpchess","pirouetti","pixel","reillycraig","riohacha","shapes","spatial","staunty","tatiana"];
+
+export { boardOptions, pieceSetOptions };
