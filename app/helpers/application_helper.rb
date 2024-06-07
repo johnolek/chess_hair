4381f9feef11c765ecdac704f558d496eed94b83
johnolek <johnoleksowicz@icloud.com>
2024-06-07 15:33:55 -0500

    Add helper for including body attributes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def body_data_attributes(attributes)
+    data_attributes = {}
+
+    attributes.each do |key, value|
+      if value.present?
+        data_attributes["data-#{key}"] = value
+      end
+    end
+
+    data_attributes
+  end
 end
