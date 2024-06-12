7403c1d58ca8240cad394db5c152d64707f9e8b9
johnolek <johnoleksowicz@icloud.com>
2024-06-12 16:38:54 -0500

    Remove turbo

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,13 +1,10 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from 'app/javascript/src', under: 'src', to: 'src'
-pin 'daily_games', to: 'app/javascript/dist/daily_games.js'
 pin_all_from 'app/javascript/dist', under: 'dist', to: 'dist'
+pin 'daily_games', to: 'app/javascript/dist/daily_games.js'
 pin "lichess-pgn-viewer", to: 'lichess-pgn-viewer.min.js'
 pin 'bootstrap', to: 'bootstrap.bundle.min.js'
 pin 'chessground', to: 'chessground/chessground.js'
