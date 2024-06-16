b1e98d8680762c44776fe2add45878023c0f5734
johnolek <johnoleksowicz@icloud.com>
2024-06-16 15:26:38 -0500

    Add helper for random int

--- app/javascript/src/util.js
+++ app/javascript/src/util.js
@@ -11,6 +11,12 @@ class Util {
   static getRootCssVarValue(varName) {
     return getComputedStyle(document.documentElement).getPropertyValue(varName);
   }
+
+  static getRandomIntBetween(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
 
 export { Util };
