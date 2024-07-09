81b511c6b89a68875dab5405ed08e324b11423e4
johnolek <johnoleksowicz@icloud.com>
2024-07-08 20:25:19 -0400

    show full errors in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,8 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
+  config.consider_all_requests_local = true
+
   config.logger = ActiveSupport::Logger.new(Rails.root.join('log', 'production.log'))
    .tap { |logger| logger.formatter = ::Logger::Formatter.new }
    .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
