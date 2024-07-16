9e8152ff4230942f20f2521d5b0423b344c8c3e2
johnolek <johnoleksowicz@icloud.com>
2024-07-16 17:14:22 -0500

    Add scopes for themes

--- app/models/user_puzzle_history.rb
+++ app/models/user_puzzle_history.rb
@@ -1,3 +1,13 @@
 class UserPuzzleHistory < ApplicationRecord
   belongs_to :user
+
+  scope :for_puzzle_id, ->(puzzle_id) { where(puzzle_id: puzzle_id) if puzzle_id.present? }
+
+  scope :minimum_rating, ->(rating) { where('rating >= ?', rating) }
+  scope :maximum_rating, ->(rating) { where('rating <= ?', rating) }
+
+  scope :solved_correctly, -> { where(win: true) }
+  scope :solved_incorrectly, -> { where(win: false) }
+  scope :with_theme, ->(theme) { where('themes LIKE ?', "%#{theme}%") }
+  scope :without_theme, ->(theme) { where.not('themes LIKE ?', "%#{theme}%") }
 end
