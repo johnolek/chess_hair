10c0e4ed59e71ed0448c6af1c4586a833799b27e
johnolek <johnoleksowicz@icloud.com>
2024-07-22 15:35:45 -0500

    Add admin flag to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_21_195146) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_22_203500) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -83,6 +83,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_21_195146) do
     t.text "active_puzzle_ids"
     t.string "lichess_code"
     t.string "lichess_code_verifier"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
