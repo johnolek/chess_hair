2998bd913323c9f6c574fbe85ddee1edacb0e682
johnolek <johnoleksowicz@icloud.com>
2024-07-27 08:53:56 -0500

    Allow ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.hosts << "localhost"
+  config.hosts << "adversely-firm-crane.ngrok-free.app"
 end
