bf273586de6b34504cd361ab3206a50d8f6bf129
johnolek <johnoleksowicz@icloud.com>
2024-07-30 11:32:19 -0500

    Add rails API abstraction

---
+++ svelte/railsApi.js
@@ -0,0 +1,71 @@
+import { Util } from "../app/javascript/src/util";
+
+const BASE_URL = "/api/v1";
+
+async function apiCall(endpoint, options = {}) {
+  const defaultHeaders = {
+    "Content-Type": "application/json",
+  };
+  options.headers = { ...defaultHeaders, ...options.headers };
+
+  const response = await Util.fetch(`${BASE_URL}${endpoint}`, options);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch ${endpoint}`);
+  }
+  return await response.json();
+}
+
+async function getApiCall(endpoint, headers = {}) {
+  return await apiCall(endpoint, { headers });
+}
+
+async function postApiCall(endpoint, body, headers = {}) {
+  return await apiCall(endpoint, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(body),
+  });
+}
+
+async function putApiCall(endpoint, body, headers = {}) {
+  return await apiCall(endpoint, {
+    method: "PUT",
+    headers,
+    body: JSON.stringify(body),
+  });
+}
+
+async function deleteApiCall(endpoint, headers = {}) {
+  return await apiCall(endpoint, {
+    method: "DELETE",
+    headers,
+  });
+}
+
+export async function fetchActivePuzzles() {
+  return await getApiCall("/user/active-puzzles");
+}
+
+export async function fetchAllPuzzles() {
+  return await getApiCall("/user/all-puzzles");
+}
+
+export async function fetchFilteredPuzzles() {
+  return await getApiCall("/user/all-filtered-puzzles");
+}
+
+export async function updateSetting(settingKey, value) {
+  return await putApiCall(`/settings/${settingKey}`, { value });
+}
+
+export async function addCurrentPuzzleToFavorites(currentPuzzle) {
+  return await postApiCall(`/user/add-favorite/${currentPuzzle.id}`, {});
+}
+
+export async function removeCurrentPuzzleFromFavorites(currentPuzzle) {
+  return await deleteApiCall(`/user/remove-favorite/${currentPuzzle.id}`);
+}
+
+export async function triggerPuzzleImport() {
+  return await postApiCall("/user/import-new-puzzle-histories", {});
+}
