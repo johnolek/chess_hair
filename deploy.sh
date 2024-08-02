c3cbbf77c0ac88ff2386657ec0100870b31192f3
johnolek <johnoleksowicz@icloud.com>
2024-08-02 02:45:52 -0500

    Probably a bad idea

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+
+set -e
+
 export RAILS_ENV=production
 export NODE_ENV=production
+
 git pull
-bundle install
-rails db:migrate
-npm ci
+
+npm install
 npm run build
 npm run prod-styles
+
+bundle install
+
+rails db:migrate
 rails assets:clobber
 rails assets:precompile
 touch tmp/restart.txt
