e00da747ce52752ae4caad0a5bb086eed7e41e70
johnolek <johnoleksowicz@icloud.com>
2024-08-03 16:20:13 -0500

    Add method for calculating material counts

--- svelte/components/lib/chess_functions.js
+++ svelte/components/lib/chess_functions.js
@@ -16,3 +16,48 @@ export function getLegalMovesMap(fen) {
   });
   return legalMovesMap;
 }
+
+const pieceValues = {
+  p: 1, // pawn
+  n: 3, // knight
+  b: 3, // bishop
+  r: 5, // rook
+  q: 9, // queen
+};
+
+export function getMaterialCounts(fen) {
+  const chess = new Chess(fen);
+  const materialCounts = {
+    w: { p: 0, n: 0, b: 0, r: 0, q: 0 },
+    b: { p: 0, n: 0, b: 0, r: 0, q: 0 },
+  };
+
+  const board = chess.board();
+  board.forEach((row) => {
+    row.forEach((square) => {
+      if (square) {
+        const piece = square;
+        const type = piece.type;
+        if (type !== "k") {
+          materialCounts[piece.color][type] += 1;
+        }
+      }
+    });
+  });
+
+  // Calculate total material value for white and black
+  const totalMaterial = {
+    w: Object.entries(materialCounts.w).reduce(
+      (total, [type, count]) => total + pieceValues[type] * count,
+      0,
+    ),
+    b: Object.entries(materialCounts.b).reduce(
+      (total, [type, count]) => total + pieceValues[type] * count,
+      0,
+    ),
+  };
+  materialCounts.w.total = totalMaterial.w;
+  materialCounts.b.total = totalMaterial.b;
+
+  return { materialCounts };
+}
