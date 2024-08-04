787f1d4e7649ae387bcb9ee8fc195cae9dd48af1
johnolek <johnoleksowicz@icloud.com>
2024-08-03 20:48:31 -0500

    Add material counter component

---
+++ svelte/components/MaterialCounter.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { getMaterialCounts } from "./lib/chess_functions"; // Adjust the path as necessary
+  import { writable } from "svelte/store";
+  import { tweened } from "svelte/motion";
+
+  let currentLoaded = false;
+  let currentCounts = writable({});
+  export let fen;
+
+  const difference = tweened(0, { duration: 400 });
+
+  $: {
+    if (fen) {
+      currentLoaded = true;
+      const counts = getMaterialCounts(fen).materialCounts;
+      $currentCounts = counts;
+      difference.set(counts.difference);
+    }
+  }
+</script>
+
+{#if currentLoaded}
+  <span
+    class="tag"
+    class:is-white={$difference > 0}
+    class:is-black={$difference < 0}
+    class:is-grey={$difference === 0}
+  >
+    {#if $difference !== 0}
+      {$difference > 0 ? "+" : ""}{$difference.toFixed(1)}
+    {:else}
+      ==
+    {/if}
+  </span>
+{/if}
