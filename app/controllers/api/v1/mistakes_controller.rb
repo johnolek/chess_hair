c03ca87430c1f7287da7b0439330b8d2d1c5d955
johnolek <johnoleksowicz@icloud.com>
2024-08-05 21:54:56 -0500

    Add statuses to mistakes controller

--- app/controllers/api/v1/mistakes_controller.rb
+++ app/controllers/api/v1/mistakes_controller.rb
@@ -4,12 +4,12 @@ module Api
       def create
         user_puzzle = @user.user_puzzles.find_by(id: mistake_params[:user_puzzle_id])
         if user_puzzle.nil?
-          render json: { message: 'user puzzle not found' }
+          render json: { message: 'user puzzle not found' }, status: :not_found
           return
         end
         mistake = user_puzzle.mistakes.new(mistake_params)
         if mistake.save
-          render json: { message: 'mistake saved' }
+          render json: { message: 'mistake saved' }, status: :created
         else
           render json: { message: 'failed to save mistake' }
         end
