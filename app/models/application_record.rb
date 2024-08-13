1bd98542773af579d34eca44f33e07babc4869be
johnolek <johnoleksowicz@icloud.com>
2024-08-12 21:36:42 -0500

    Add helper for more efficient finding of random records

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,17 @@ class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
   scope :random_order, -> { order(Arel.sql('RANDOM()')) }
+
+  class << self
+    def random_item_from_relation(relation)
+      min = relation.minimum(:id)
+      max = relation.maximum(:id)
+      if !min || !max
+        return relation.random_order.first
+      end
+
+      random = rand(min..max)
+      relation.where(id: random..).limit(1).first
+    end
+  end
 end
