7a05dd4d025c57298999d2e96bb91c8f219063a4
johnolek <johnoleksowicz@icloud.com>
2024-08-12 22:11:20 -0500

    Add convenience methods

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -4,4 +4,14 @@ class Collection < ApplicationRecord
   has_and_belongs_to_many :user_puzzles
 
   validates :name, presence: true, uniqueness: { scope: :user_id }
+
+  def add_puzzle(puzzle)
+    user_puzzles << puzzle
+    save!
+  end
+
+  def remove_puzzle(puzzle)
+    user_puzzles.delete(puzzle)
+    save!
+  end
 end
