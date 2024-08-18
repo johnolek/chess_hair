965aba2ec33b96118139c152e5baf0906bf26f0d
johnolek <johnoleksowicz@icloud.com>
2024-08-18 14:55:24 -0500

    Add scopes for with and without theme

--- app/models/lichess_puzzle.rb
+++ app/models/lichess_puzzle.rb
@@ -3,6 +3,9 @@ class LichessPuzzle < ApplicationRecord
 
   scope :high_quality, -> { where(rating_deviation: ..80, popularity: 80.., nb_plays: 1000..) }
 
+  scope :with_theme, ->(theme) { where(arel_table[:themes].matches("%#{theme}%")) }
+  scope :without_theme, ->(theme) { where.not(arel_table[:themes].matches("%#{theme}%")) }
+
   scope :excluding_user_puzzles, ->(user) do
     return all if user.user_puzzles.empty?
     where.not(puzzle_id: user.user_puzzles.select(:lichess_puzzle_id))
