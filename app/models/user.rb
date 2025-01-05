b88d452b89b1c59637c7bc8fee0a9f77b2a43734
johnolek <johnoleksowicz@icloud.com>
2025-01-05 11:55:11 -0600

    Disable open registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
   has_many :user_puzzles
   has_many :mistakes, through: :user_puzzles
   has_many :puzzle_results, through: :user_puzzles
