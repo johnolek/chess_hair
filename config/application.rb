e7e6e3c1a2bc80a3990fec06cccbfd1d69c89d0d
johnolek <johnoleksowicz@icloud.com>
2025-02-08 12:54:39 -0600

    Fix timezone?

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,17 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -23,12 +23,12 @@ module ChessHair
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'America/Chicago'
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -46,7 +46,7 @@ module ChessHair
 
     config.public_file_server.headers = {
       'Cross-Origin-Embedder-Policy' => 'require-corp',
-      'Cross-Origin-Opener-Policy' => 'same-origin',
+      'Cross-Origin-Opener-Policy' => 'same-origin'
     }
   end
 end
