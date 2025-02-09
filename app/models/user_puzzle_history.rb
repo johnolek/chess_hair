68ed4b2ef214b836bd1f8bd90084f37741194c4b
johnolek <johnoleksowicz@icloud.com>
2025-02-08 21:36:21 -0600

    add played at datetime to user puzzle histories

--- app/models/user_puzzle_history.rb
+++ app/models/user_puzzle_history.rb
@@ -1,7 +1,7 @@
 class UserPuzzleHistory < ApplicationRecord
   belongs_to :user
 
-  scope :for_puzzle_id, ->(puzzle_id) { where(puzzle_id: puzzle_id) if puzzle_id.present? }
+  scope :for_puzzle_id, ->(puzzle_id) { where(puzzle_id:) if puzzle_id.present? }
 
   scope :solved_correctly, -> { where(win: true) }
   scope :solved_incorrectly, -> { where(win: false) }
@@ -14,14 +14,19 @@ class UserPuzzleHistory < ApplicationRecord
   def create_user_puzzle
     return unless lichess_puzzle.present? && lichess_puzzle.fen
     return if user.user_puzzles.exists?(fen: lichess_puzzle.fen)
+
     collection = user.failed_lichess_puzzles_collection
     new_puzzle = user.user_puzzles.create!(
       lichess_puzzle_id: puzzle_id,
       lichess_rating: lichess_puzzle.rating,
       uci_moves: lichess_puzzle.moves,
-      fen: lichess_puzzle.fen,
+      fen: lichess_puzzle.fen
     )
     collection.user_puzzles << new_puzzle
     collection.save!
+  end
+
+  def played_at_datetime
+    Time.at(played_at / 1000)
   end
 end
