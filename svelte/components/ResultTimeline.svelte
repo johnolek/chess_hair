59f915690aa40caa3cbb237161546e761cfb1c4e
johnolek <johnoleksowicz@icloud.com>
2025-02-08 21:52:03 -0600

    Improve styling of result timeline

--- svelte/components/ResultTimeline.svelte
+++ svelte/components/ResultTimeline.svelte
@@ -59,60 +59,67 @@
   }
 </script>
 
-<div class="scrollable mb-0" bind:this={scrollable}>
-  <div class="left-indicator"></div>
-  <div class="timeline">
-    <span class="timeline-label">History:</span>
-    <svg 
-      class="timeline-svg" 
-      height={CIRCLE_RADIUS * 2 + 4} 
-      width={results.length * CIRCLE_SPACING}>
-      <defs>
+<div class="timeline-container">
+<span class="timeline-label">History:</span>
+  <div class="scrollable mb-0" bind:this={scrollable}>
+    <div class="left-indicator"></div>
+    <div class="timeline">
+      <svg 
+        class="timeline-svg" 
+        height={CIRCLE_RADIUS * 2 + 4} 
+        width={results.length * CIRCLE_SPACING}>
+        <defs>
+          {#each sortedResults as result, index}
+            {#if index < results.length - 1}
+              <linearGradient id="gradient-{index}" x1="0%" y1="0%" x2="100%" y2="0%">
+                <stop offset="0%" stop-color={getResultColor(result)} />
+                <stop offset="100%" stop-color={getResultColor(sortedResults[index + 1])} />
+              </linearGradient>
+            {/if}
+          {/each}
+        </defs>
         {#each sortedResults as result, index}
           {#if index < results.length - 1}
-            <linearGradient id="gradient-{index}" x1="0%" y1="0%" x2="100%" y2="0%">
-              <stop offset="0%" stop-color={getResultColor(result)} />
-              <stop offset="100%" stop-color={getResultColor(sortedResults[index + 1])} />
-            </linearGradient>
+            <rect 
+              in:fade={{ duration: mounted ? 300 : 0 }} 
+              x={CIRCLE_SPACING * index + CIRCLE_RADIUS + (CIRCLE_RADIUS - 3.5)}
+              y={CIRCLE_RADIUS + 2 - 2}
+              width={CIRCLE_SPACING - ((CIRCLE_RADIUS - 3.5) * 2)}
+              height="4"
+              fill={`url(#gradient-${index})`}
+              rx="2"
+              ry="2"
+            />
           {/if}
         {/each}
-      </defs>
-      {#each sortedResults as result, index}
-        {#if index < results.length - 1}
-          <rect 
-            in:fade={{ duration: mounted ? 300 : 0 }} 
-            x={CIRCLE_SPACING * index + CIRCLE_RADIUS + (CIRCLE_RADIUS - 3.5)}
-            y={CIRCLE_RADIUS + 2 - 2}
-            width={CIRCLE_SPACING - ((CIRCLE_RADIUS - 3.5) * 2)}
-            height="4"
-            fill={`url(#gradient-${index})`}
-            rx="2"
-            ry="2"
-          />
-        {/if}
-      {/each}
-      {#each sortedResults as result, index}
-        <g 
-          in:fade={{ duration: mounted ? 600 : 0 }} 
-          use:scrollIntoView>
-          <circle
-            cx={CIRCLE_SPACING * index + CIRCLE_RADIUS}
-            cy={CIRCLE_RADIUS + 2}
-            r={CIRCLE_RADIUS - 2}
-            stroke={getResultColor(result)}
-            stroke-width="4"
-            fill="transparent"
-            class="timeline-circle"
-          >
-            <title>{formatDate(result.created_at)}</title>
-          </circle>
-        </g>
-      {/each}
-    </svg>
+        {#each sortedResults as result, index}
+          <g 
+            in:fade={{ duration: mounted ? 600 : 0 }} 
+            use:scrollIntoView>
+            <circle
+              cx={CIRCLE_SPACING * index + CIRCLE_RADIUS}
+              cy={CIRCLE_RADIUS + 2}
+              r={CIRCLE_RADIUS - 2}
+              stroke={getResultColor(result)}
+              stroke-width="4"
+              fill="transparent"
+              class="timeline-circle"
+            >
+              <title>{formatDate(result.created_at)}</title>
+            </circle>
+          </g>
+        {/each}
+      </svg>
+    </div>
   </div>
 </div>
 
 <style>
+  .timeline-container {
+    display: flex;
+    align-items: center;
+  }
+
   .timeline {
     display: flex;
     align-items: center;
@@ -121,14 +128,15 @@
   }
 
   .timeline-label {
-    margin-right: 16px;
     font-weight: 500;
+    display: inline-block;
   }
 
   .scrollable {
     overflow-x: auto;
     padding: 0 16px;
     position: relative;
+    display: inline-block;
   }
 
   .timeline-svg {
